9c486277ef58781026979d79d5c97e8cb8909d6a
Antonino Manueli <antonino.manueli@outlook.com>
2013-01-03 14:02:39 +0100

    Grundaufbau der Klasse Cell implementiert. Koordinaten, Belegung und Farbe des Steines (bei Belegung).
    Change-Id: I8a6d2f5853ae41e9dfea8c94d8ca66470a4892ed
    Signed-off-by: Antonino Manueli <antonino.manueli@outlook.com>

--- de.fhkoeln.reversi/src/de/fhkoeln/reversi/model/Cell.scala
+++ de.fhkoeln.reversi/src/de/fhkoeln/reversi/model/Cell.scala
@@ -1,5 +1,15 @@
 package de.fhkoeln.reversi.model
 
-class Cell {
+/**
+ * 
+ * x: Zeilen / column
+ * y: Spalten / row
+ * isUsed: Ist die Zelle mit einem Stein belegt? --> true: belegt; false: unbelegt
+ *         Is there a token on the cell? --> true: occupied; false: unoccupied 
+ * isWhite: Liegt ein weißer Stein auf dem Feld? --> true: weißer Stein; false: schwarzer Stein
+ *          Is there a white token on the cell? --> true: white token; false: black token
+ */
+class Cell( val x: Int, val y: Int, val isUsed: Boolean, val isWhite: Boolean ) {
   
+  override def toString = ("Cell( x= " + x + ", y= " + y + ", isUsed= " + isUsed + ", isWhite= " + isWhite + ")" )
 }
