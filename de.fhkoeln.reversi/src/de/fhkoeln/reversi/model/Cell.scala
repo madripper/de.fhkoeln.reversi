f457bbad0ea85eb8e60761d60d60fb8498479950
Antonino Manueli <antonino.manueli@outlook.com>
2013-01-03 16:29:37 +0100

    Variable isUsed in isSet umbenannt. Kommentare entsprechend angepasst. Variable isBlack hinzugefügt. Kommentare für isWhite/isBlack bei false der Logik entsprechend angepasst. x/y getauscht. Die Methode toString so angepasst, dass bei einem weißen Token ein 'o' erscheint, bei einem schwarzen Token ein '*' und bei einer leeren Zelle ' '.
    Change-Id: I673d039a76af523f609b38891f0287859e14e871
    Signed-off-by: Antonino Manueli <antonino.manueli@outlook.com>

--- de.fhkoeln.reversi/src/de/fhkoeln/reversi/model/Cell.scala
+++ de.fhkoeln.reversi/src/de/fhkoeln/reversi/model/Cell.scala
@@ -2,14 +2,23 @@ package de.fhkoeln.reversi.model
 
 /**
  * 
- * x: Zeilen / column
- * y: Spalten / row
- * isUsed: Ist die Zelle mit einem Stein belegt? --> true: belegt; false: unbelegt
+ * x: Spalten / row
+ * y: Zeilen / column
+ * isSet: Ist die Zelle mit einem Stein belegt? --> true: belegt; false: unbelegt
  *         Is there a token on the cell? --> true: occupied; false: unoccupied 
- * isWhite: Liegt ein weißer Stein auf dem Feld? --> true: weißer Stein; false: schwarzer Stein
- *          Is there a white token on the cell? --> true: white token; false: black token
+ * isWhite: Liegt ein weißer Stein auf dem Feld? --> true: weißer Stein; false: Wenn isSet == true: schwarzer Stein. Sonst: unbelegt.
+ *          Is there a white token on the cell? --> true: white token; false: when isSet == true, then black token, else unoccupied
+ * isBlack: Liegt ein schwarzer Stein auf dem Feld? --> true: schwarzer Stein; flase: Wenn isSet == true: weißer Stein. Sonst: unbelegt.
+ *          Is there a black token on the cell? --> true: black token; false: when isSet == true, then white token, else unoccupied 
  */
-class Cell( val x: Int, val y: Int, val isUsed: Boolean, val isWhite: Boolean ) {
-  
-  override def toString = ("Cell( x= " + x + ", y= " + y + ", isUsed= " + isUsed + ", isWhite= " + isWhite + ")" )
+class Cell( val x: Int, val y: Int, val isSet: Boolean, val isWhite: Boolean ) {
+  val isBlack = !isWhite && isSet
+  var overrideString: String = " "
+    
+  if( isSet ) {
+    if( isWhite ) overrideString = "o" else overrideString = "*"
+  }
+  override def toString = ( overrideString )
 }
+
+
