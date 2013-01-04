e42f12d81537b71c0c0cb61046d2cf73ec13aaef
Antonino Manueli <antonino.manueli@outlook.com>
2013-01-04 09:34:52 +0100

    Neudefinierung der Klasse. Entfernen der x,y-Koordinaten, entfernen von seperaten Variablen für einen weißen/schwarzen Spielstein. Rückgabestring durch switch/case realisiert(vorher if). isSet durch isEmpty ersetzt - sinniger.
    Change-Id: I3ea0e6ff16c9c72ea17cae0574013f826ee9254c
    Signed-off-by: Antonino Manueli <antonino.manueli@outlook.com>

--- de.fhkoeln.reversi/src/de/fhkoeln/reversi/model/Cell.scala
+++ de.fhkoeln.reversi/src/de/fhkoeln/reversi/model/Cell.scala
@@ -1,23 +1,23 @@
 package de.fhkoeln.reversi.model
 
 /**
- * 
- * x: Spalten / row
- * y: Zeilen / column
- * isSet: Ist die Zelle mit einem Stein belegt? --> true: belegt; false: unbelegt
- *         Is there a token on the cell? --> true: occupied; false: unoccupied 
- * isWhite: Liegt ein weißer Stein auf dem Feld? --> true: weißer Stein; false: Wenn isSet == true: schwarzer Stein. Sonst: unbelegt.
- *          Is there a white token on the cell? --> true: white token; false: when isSet == true, then black token, else unoccupied
- * isBlack: Liegt ein schwarzer Stein auf dem Feld? --> true: schwarzer Stein; flase: Wenn isSet == true: weißer Stein. Sonst: unbelegt.
- *          Is there a black token on the cell? --> true: black token; false: when isSet == true, then white token, else unoccupied 
+ * isEmpty: Ist die Zelle mit einem Stein belegt? --> true: belegt; false: unbelegt
+ *          Is there a token on the cell? --> true: occupied; false: unoccupied 
  */
-class Cell( val x: Int, val y: Int, val isSet: Boolean, val isWhite: Boolean ) {
-  val isBlack = !isWhite && isSet
+class Cell( val colour: Char ) {
+  var isEmpty: Boolean = true
   var overrideString: String = " "
-    
-  if( isSet ) {
-    if( isWhite ) overrideString = "o" else overrideString = "*"
+  var token: Char = colour
+  
+  colour match {
+    case 'W' => overrideString = "W" // Weißer Stein / white token
+                isEmpty = false
+    case 'B' => overrideString = "B" // Schwarzer Stein / black token
+                isEmpty = false
+    case '-' => overrideString = " " // kein Stein / no token
+      			isEmpty = true
   }
+  
   override def toString = ( overrideString )
 }
 
