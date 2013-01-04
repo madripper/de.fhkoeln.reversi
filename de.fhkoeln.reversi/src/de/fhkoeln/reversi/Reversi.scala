bd1c685004850471b4b47d4d1791d6104e9ced06
Antonino Manueli <antonino.manueli@outlook.com>
2013-01-04 16:54:59 +0100

    Cell als Vektor an Grid übergeben.
    Change-Id: Ib15b7110a0691a1e6dc569bd9ace8ffa5d5c8a37
    Signed-off-by: Antonino Manueli <antonino.manueli@outlook.com>

--- de.fhkoeln.reversi/src/de/fhkoeln/reversi/Reversi.scala
+++ de.fhkoeln.reversi/src/de/fhkoeln/reversi/Reversi.scala
@@ -5,8 +5,9 @@ import de.fhkoeln.reversi.controller.ReversiController
 import de.fhkoeln.reversi.view.tui.Tui
 
 class Reversi {
-  val controller = new ReversiController( new Grid( 8 )) // neuen Controller erzeugen mit einem 8x8-Feld
-  val tui = new Tui( controller )	// neue Text-UI
+  var cell = new Cell( '-' )
+  var controller = new ReversiController( new Grid( Vector(  ) )) // neuen Controller erzeugen mit einem 8x8-Feld
+  var tui = new Tui( controller )	// neue Text-UI
   def main(args: Array[String]) {
     while (tui.processInputLine(readLine())) {}
   }
