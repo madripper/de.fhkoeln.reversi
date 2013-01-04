e44ca0776ce81681db1663100c02a699d057e51d
Antonino Manueli <antonino.manueli@outlook.com>
2013-01-04 11:53:43 +0100

    Rumpfmethoden für den Controller.
    Change-Id: Id4b6161db166604e526f29b813e466ce15631937
    Signed-off-by: Antonino Manueli <antonino.manueli@outlook.com>

---
+++ de.fhkoeln.reversi/src/de/fhkoeln/reversi/controller/ReversiController.scala
@@ -0,0 +1,14 @@
+package de.fhkoeln.reversi.controller
+import de.fhkoeln.reversi.model.Grid
+import scala.swing.Publisher
+
+class ReversiController( var grid : Grid ) extends Publisher {
+
+  def reset {
+    
+  }
+  
+  def setCell( x: Int, y: Int ) {
+    
+  }
+}
