5883bc7c3064e16fd5a567f276b2dbbc3968c9a0
Antonino Manueli <antonino.manueli@outlook.com>
2013-01-05 18:00:52 +0100

    Zuweisung von token in der match/case Anweisung.
    Change-Id: Ifb5cde18b494f1c06103a2ae39663dc59d9d006b
    Signed-off-by: Antonino Manueli <antonino.manueli@outlook.com>

--- de.fhkoeln.reversi/src/de/fhkoeln/reversi/model/Cell.scala
+++ de.fhkoeln.reversi/src/de/fhkoeln/reversi/model/Cell.scala
@@ -4,20 +4,24 @@ package de.fhkoeln.reversi.model
  * isEmpty: Ist die Zelle mit einem Stein belegt? --> true: belegt; false: unbelegt
  *          Is there a token on the cell? --> true: occupied; false: unoccupied 
  */
-class Cell( val colour: Char ) {
+class Cell( colour: Char ) {
   var isEmpty: Boolean = true
-  var overrideString: String = " "
-  var token: Char = colour
+  var overrideString: String = "-"
+  var token: Char = '-'
   
   colour match {
     case 'W' => overrideString = "W" // Weißer Stein / white token
                 isEmpty = false
+                token = 'W'
     case 'B' => overrideString = "B" // Schwarzer Stein / black token
                 isEmpty = false
+                token = 'B'
     case '-' => overrideString = "-" // kein Stein / no token
       			isEmpty = true
+      			token = '-'
     case _   => overrideString = "-" // default: kein Stein
                 isEmpty = true
+      			token = '-'
   }
   
   override def toString = ( overrideString )
