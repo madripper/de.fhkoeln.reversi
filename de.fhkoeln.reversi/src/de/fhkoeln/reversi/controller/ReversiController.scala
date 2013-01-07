0bdc6f698651b6c8f913e7034f6c02f64334cff0
Antonino Manueli <antonino.manueli@outlook.com>
2013-01-07 08:29:53 +0100

    init hinzugefügt - Befüllen des Feldes mit default-Werten. reset implementiert. setCell um Abfrage erweitert, ob das Spielfeld leer ist.

--- de.fhkoeln.reversi/src/de/fhkoeln/reversi/controller/ReversiController.scala
+++ de.fhkoeln.reversi/src/de/fhkoeln/reversi/controller/ReversiController.scala
@@ -4,12 +4,44 @@ import scala.swing.Publisher
 
 class ReversiController( var grid : Grid ) extends Publisher {
 
-  def reset {
+  var switchPlayer: Boolean = false
     
+  /**
+   * <pre><b><i>init</i></b>
+   * This Method initilize the playboard 
+   * with the default values.
+   * </pre>
+   */  
+  def init {
+    grid.update( 4, 4, 'W' )
+    grid.update( 5, 5, 'W' )
+    grid.update( 4, 5, 'B' )
+    grid.update( 5, 4, 'B' )
   }
   
-  def setCell( column: Int, row: Int ) {
-    grid.update( column, row, 'W' )
-    println( grid.toString )
+  def reset {
+    grid = new Grid( 8 )
   }
+  
+  def setCell( column: Int, row: Int ): Boolean = {
+    if( grid.isFieldEmpty(column, row) ) {
+      grid.update( column, row, if( switchPlayer ) 'B' else 'W' )    
+      switchPlayer = !switchPlayer
+      true 
+    } 
+    else {
+      println( "Field is not empty. Try another Field.." )
+      false
+    }
+  }
+  
+  def isFieldEmpty( column: Int, row: Int ): Boolean = {
+    grid.isFieldEmpty(column, row)
+  }
+  
+  def getTokenFrom( column: Int, row: Int ): Char = {
+    grid.getTokenFrom(column, row)
+  }
+  
+  override def toString = ( grid.toString )
 }
