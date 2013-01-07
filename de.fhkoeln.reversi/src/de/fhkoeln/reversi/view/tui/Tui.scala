536aede2669383f410635022cf34153d95b73188
Antonino Manueli <antonino.manueli@outlook.com>
2013-01-07 08:28:27 +0100

    Anpassen der Tui. Konsolenausgaben an sinnvollen Stellen, Methodenaufruf zum setzen von Zellen angepasst.

--- de.fhkoeln.reversi/src/de/fhkoeln/reversi/view/tui/Tui.scala
+++ de.fhkoeln.reversi/src/de/fhkoeln/reversi/view/tui/Tui.scala
@@ -1,30 +1,47 @@
 package de.fhkoeln.reversi.view.tui
 
-import de.fhkoeln.reversi.controller.ReversiController
+import de.fhkoeln.reversi.controller.ReversiArrayController
 import scala.io.Source._
 import scala.swing.Reactor
 
-class Tui( var controller: ReversiController ) extends Reactor {
-  listenTo( controller )
+/**
+ * <pre>
+ * <b>Class <i>Tui</i></b>
+ *
+ * </pre>
+ */
+class Tui(var controller: ReversiArrayController) extends Reactor {
+  listenTo(controller)
   printTui
+
+  /**
+   * <pre>
+   * <b><i>printTui</i></b>
+   * Method generates the output for the console.
+   * </pre>
+   */
   def printTui = {
-    println( controller.grid.toString )
+    controller.init
+    println(controller.toString)
     println("Enter command: q-Quit n-New xy-SetCell")
+    println(if (controller.switchPlayer) "black player's turn: " else "white player's turn:")
   }
- def processInputLine(input: String) = {
-   var continue = true
-   input match {
+
+  def processInputLine(input: String) = {
+    var continue = true
+    input match {
       case "q" => continue = false
       case "n" => controller.reset
       case _ => {
         input.toList.filter(c => c != ' ').map(c => c.toString.toInt) match {
           case column :: row :: Nil => {
-            controller.setCell(column, row)
+            controller.setCell(column-1, row-1)
+            printTui
           }
-          case _ => println("False Input!!!")
+          case _ => println("False Input! Input-Format: 'xy' or 'x y'")
         }
       }
     }
-   continue
- }
+    continue
+  }
 }
