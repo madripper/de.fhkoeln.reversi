d3f905f690f1bb05d7a6ca11baf8e04a552250f2
Antonino Manueli <antonino.manueli@outlook.com>
2013-01-07 08:35:49 +0100

    Angepasst an neue Array-Struktur des Spielfeldes.

--- de.fhkoeln.reversi/src/de/fhkoeln/reversi/Reversi.scala
+++ de.fhkoeln.reversi/src/de/fhkoeln/reversi/Reversi.scala
@@ -1,11 +1,11 @@
 package de.fhkoeln.reversi
 
-import de.fhkoeln.reversi.model.Grid
-import de.fhkoeln.reversi.controller.ReversiController
+import de.fhkoeln.reversi.model.Playboard
+import de.fhkoeln.reversi.controller.ReversiArrayController
 import de.fhkoeln.reversi.view.tui.Tui
 
 object Reversi {
-  val controller = new ReversiController( new Grid(8)) // neuen Controller erzeugen mit einem 8x8-Feld
+  val controller = new ReversiArrayController( new Playboard(8)) // neuen Controller erzeugen mit einem 8x8-Feld
   var tui = new Tui( controller )	// neue Text-UI
   def main(args: Array[String]) {
     while (tui.processInputLine(readLine())) {}
