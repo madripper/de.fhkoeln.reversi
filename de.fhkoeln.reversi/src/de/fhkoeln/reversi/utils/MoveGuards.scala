c356fc87d2a7bfb370486d3804267b8d4b942e35
Antonino Manueli <antonino.manueli@outlook.com>
2013-01-07 17:35:19 +0100

    limitCheck für Boundaries hinzugefügt. column, row angepasst - war verdreht.

--- de.fhkoeln.reversi/src/de/fhkoeln/reversi/utils/MoveGuards.scala
+++ de.fhkoeln.reversi/src/de/fhkoeln/reversi/utils/MoveGuards.scala
@@ -6,14 +6,15 @@ trait MoveGuards {
   val lowerLimit, rightLimit = 7
 
   // Guards that check that the given indeces are within the board limits
-  val upLeftDiagonalCheck = (i: Int, j: Int) => i >= upperLimit && j >= leftLimit 
-  val upRightDiagonalCheck = (i: Int, j: Int) => i >= upperLimit && j <= rightLimit
-  val downRightDiagonalCheck = (i: Int, j: Int) => i <= lowerLimit && j <= rightLimit
-  val downLeftDiagonalCheck = (i: Int, j: Int) => i <= lowerLimit && j >= leftLimit
-  val leftCheck = (i: Int, j: Int) => j >= leftLimit 
-  val upCheck = (i: Int, j: Int) => i >= upperLimit 
-  val rightCheck = (i: Int, j: Int) => j <= rightLimit 
-  val downCheck = (i: Int, j: Int) => i <= lowerLimit
+  val upLeftDiagonalCheck = (column: Int, row: Int) => column >= leftLimit && row >= upperLimit 	//1
+  val upRightDiagonalCheck = (column: Int, row: Int) => column <= rightLimit && row >= upperLimit //2
+  val downRightDiagonalCheck = (column: Int, row: Int) => column <= rightLimit && row <= lowerLimit//3
+  val downLeftDiagonalCheck = (column: Int, row: Int) => column >= leftLimit && row <= lowerLimit  //4
+  val leftCheck = (column: Int, row: Int) => column >= leftLimit //5
+  val upCheck = (column: Int, row: Int) => row >= upperLimit //6
+  val rightCheck = (column: Int, row: Int) => column <= rightLimit //7
+  val downCheck = (column: Int, row: Int) => row <= lowerLimit//8
+  val limitCheck = (column: Int, row: Int) => leftLimit <= column && column <= rightLimit && upperLimit <= row && row <= lowerLimit
 
   // Directional movements
   val left, up = (x: Int) => x - 1
