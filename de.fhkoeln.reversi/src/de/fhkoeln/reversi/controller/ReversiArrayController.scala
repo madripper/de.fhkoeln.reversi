04527ab5f8aca037023b20631a413c2d4ed2bb0e
Antonino Manueli <antonino.manueli@outlook.com>
2013-01-07 08:35:21 +0100

    Abstraktion von vorherigem ReversiController, angepasst auf neue Array-Struktur des Spielfeldes. Methode init angepasst. Befüllung skaliert mit Spielfeldgröße. (Immer mittige Befüllung) Methode cantMove hinzugefügt. Soll ein hin und her zwischen weißem und schwarzem Spieler vermeiden. Variable für Spielzugnummer hinzugefügt.

---
+++ de.fhkoeln.reversi/src/de/fhkoeln/reversi/controller/ReversiArrayController.scala
@@ -0,0 +1,66 @@
+package de.fhkoeln.reversi.controller
+import de.fhkoeln.reversi.model.Cell
+import scala.swing.Publisher
+import de.fhkoeln.reversi.model.Playboard
+
+class ReversiArrayController( var board : Playboard ) extends Publisher {
+
+  var switchPlayer: Boolean = false
+  var playerCanMove: Boolean = false
+  var validMove: Boolean = false
+  var ping: Boolean = false
+  var pong: Int = _
+  var turnNo: Int = _
+    
+  /**
+   * <pre><b><i>init</i></b>
+   * This Method initilize the playboard 
+   * with the default values.
+   * </pre>
+   */  
+  def init {
+    board.update( (board.x/2)-1, (board.y/2)-1, 'W' )
+    board.update( (board.x/2),   (board.y/2), 'W' )
+    board.update( (board.x/2)-1, (board.y/2), 'B' )
+    board.update( (board.x/2),   (board.y/2)-1, 'B' )
+  }
+  
+  def reset {
+    board = new Playboard( 8 )
+  }
+  
+  def setCell( column: Int, row: Int ): Boolean = {
+    val movePossible: List[Int] = board.possibleMove( column, row, switchPlayer )
+    playerCanMove = if( movePossible.isEmpty ) false else true
+    if( playerCanMove ) {
+      if( movePossible.contains(column*10+row) ) {        
+    	  turnNo = board.takeTurn( column, row, switchPlayer )
+    	  switchPlayer = !switchPlayer 
+      }
+      else {
+        println( "Invalid move. Try Again.." )
+      }
+    }
+    else ping = cantMove( turnNo )
+    
+    
+    
+    
+    false
+  }
+  
+  def cantMove( turnNo: Int ): Boolean = {
+    if( switchPlayer ) println( "black player can't move.." ) else println( "white player can't move" )
+    if( pong == turnNo ) true else { pong = turnNo; false}
+  }
+  
+  def isFieldEmpty( column: Int, row: Int ): Boolean = {
+    board.isFieldEmpty(column, row)
+  }
+  
+  def getTokenFrom( column: Int, row: Int ): Char = {
+    board.getTokenFrom(column, row)
+  }
+  
+  override def toString = ( board.toString )
+}
